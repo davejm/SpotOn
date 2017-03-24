aa71d5202692ed819cad9be3cf5ce418c2526a8d
David Moodie <davidmoodie12@gmail.com>
2017-03-24 15:06:05 +0000

    Correct spotify scope request

--- js/main.js
+++ js/main.js
@@ -68,7 +68,7 @@ function authorize(clientId, redirectUri, scope) {
   var state = generateRandomString(16);
   // localStorage.setItem(stateKey, state);
 
-  var scope = 'user-read-private user-read-email';
+  var scope = 'playlist-modify-public user-top-read';
   var url = 'https://accounts.spotify.com/authorize';
   url += '?response_type=token';
   url += '&client_id=' + encodeURIComponent(client_id);
