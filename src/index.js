d47f58360db5208157c771cc620983bfc188e7b9
David Moodie <davidmoodie12@gmail.com>
2018-03-25 20:50:30 +0100

    Remove dotenv

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import {unregister} from './serviceWorker';
 
-require('dotenv').config();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
